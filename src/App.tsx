c49d85a5d33440ce372a13a130dbcfc0f2035179
Patrick Bower <bower.patrick@gmail.com>
2018-06-01 10:44:53 +0100

    up to date

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import './Grid.css';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App black-70 grid-container">
+        <div className="pt5 bg-moon-gray">
+          <h1 className="f-headline b lh-solid mt0 tr white">40</h1>
+        </div>
+        <div className="pt5">
+          <h1 className="f-headline b lh-solid mt0 mb3">Forty</h1>
+            <p className="f3 w-60 lh-copy mt0 mb4 mb5-ns">
+              Time flies like an arrow. Fruit flies like a banana.
+            </p>
+          <ul className="list pl0 mt2 fn">
+            <li>Go to Japan</li>
+            <li>Cycling sea to sea across Lake District</li>
+            <li>Get a tattoo</li>
+            <li>Surf a wave</li>
+            <li>Run London marathon or do a triathlon</li>
+            <li>Throw an old school house party</li>
+            <li>Learn how to make sushi</li>
+            <li>Get a dog</li>
+            <li>Grow some facial hair</li>
+            <li>Start a vegetable garden</li>
+            <li>Buy something outrageous I've always wanted</li>
+            <li>Do a public speaking engagement</li>
+            <li>Learn an instrument</li>
+            <li>Reconnect with an old friend</li>
+            <li>Play a DJ gig</li>
+            <li>Print my own stickers</li>
+            <li>Go on a trip alone</li>
+            <li>Make pizza from scratch</li>
+            <li>Bury a time capsule</li>
+            <li>Start to learn a new language, go to country and use it</li>
+            <li>Do a camper van trip</li>
+            <li>Go to a game at Anfield</li>
+            <li>Print own T-shirt</li>
+            <li>Go badger watching</li>
+            <li>Climb a mountain</li>
+            <li>Plant a tree</li>
+            <li>Read a classic book</li>
+            <li>Go geocaching</li>
+            <li>Bake a cake for someone special</li>
+            <li>Visit a volcano</li>
+            <li>Solve a Rubik’s Cube</li>
+            <li>Invest in Crypto currency</li>
+            <li>Eat a raw oyster</li>
+            <li>Down a pint</li>
+            <li>Go bombing</li>
+            <li>Go gluten-free for a month</li>
+            <li>Gamble at a casino</li>
+            <li>To to Lille flee market</li>
+            <li>Sleep next to a campfire</li>
+            <li>Swim round a pier</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
