b4f5b2cea23b084871891fddc97a9cf3ffd7bbff
Patrick Bower <bower.patrick@gmail.com>
2018-06-24 22:59:44 +0100

    Basic replace of typescript
    App running as expected

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import './Grid.css';
+
+import CheckItemComponent from './CheckItemComponent';
 
 class App extends Component {
+
+  createCheckItems() {
+    const Data = require('./data.json');
+    return Data.list.map(item => {
+      return <CheckItemComponent name={item} key={item} />
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App sans-serif black-70 grid-container">
+        <div className="pt5 bg-moon-gray">
+          <h1 className="f-headline b lh-solid mt0 tr white">40</h1>
+        </div>
+        <div className="pt5">
+          <h1 className="f-headline b lh-solid mt0 mb3">Forty</h1>
+          <p className="f3 w-60 lh-copy mt0 mb4 mb5-ns">
+            Take care of the minutes, and the days will take care of themselves
+          </p>
+          <form>
+            <fieldset className="bn pa0 checklist">
+              {this.createCheckItems()}
+            </fieldset>
+          </form>
+        </div>
       </div>
     );
   }
